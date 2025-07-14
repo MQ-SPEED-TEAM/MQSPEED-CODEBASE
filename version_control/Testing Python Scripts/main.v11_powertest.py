f238c01ff08f8d7432d4f78a50aa3891e3d71cb1
MQ-SPEED-TEAM <maqspeedteam@gmail.com>
2025-07-14 16:27:56 +1000

    Updated PIN 7 - 10

--- version_control/Testing Python Scripts/main.v11_powertest.py
+++ version_control/Testing Python Scripts/main.v11_powertest.py
@@ -20,7 +20,7 @@ def system():
     #//////////////////////////////////////////////////////////////////////////////
 
     GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-    GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+    GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
     powerstate = False
     BASE_PATH = '/home/mqspeed/Desktop/'
